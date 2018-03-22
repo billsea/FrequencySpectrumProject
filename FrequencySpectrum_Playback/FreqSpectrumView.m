69d7136fcff4bc9462c7d543706c67d2b86e1b95
Bill S <bill.e.seaman@gmail.com>
2018-03-21 17:17:38 -0700

    Adjust freq spectrum bar display

--- FrequencySpectrum_Playback/FreqSpectrumView.m
+++ FrequencySpectrum_Playback/FreqSpectrumView.m
@@ -8,6 +8,7 @@
 
 #import "FreqSpectrumView.h"
 
+#define kNumberOfDisplayFrequencies 16//64 is to 10kz//256 is max/full.
 
 @implementation FreqSpectrumView
 
@@ -58,9 +59,24 @@
 	_barViews = [[NSMutableArray alloc] init];
 	//for(NSObject* fv in _frequencyValues){
 	int i = 0;
-	while (i < 256) {
+	
+	//Show only frequencies that have a value
+	float barWidth = self.frame.size.width/kNumberOfDisplayFrequencies;
+	while (i < kNumberOfDisplayFrequencies) {
 		UIView* v = [[UIView alloc] initWithFrame:CGRectZero];
 		v.backgroundColor = self.tintColor;
+		if(i==3 || i==6 || i==12){
+			//frequencies are calculated as
+			//sample rate: 44100
+			//buckets: 256
+			// 44100/256 = 172 (each bar)
+			// 1000(<<that's hz)/172 = 5.89(round to 6)
+			//1khz?
+			UILabel* fLabel = [[UILabel alloc] initWithFrame:CGRectMake(barWidth*i, self.frame.size.height + 15, 100, 15)];
+			fLabel.text = [NSString stringWithFormat:@"%dhz",i*172];
+			[self addSubview:fLabel];
+			v.backgroundColor = [UIColor redColor];
+		}
 		[_barViews addObject:v];
 		[self addSubview:v];
 		i++;
@@ -71,9 +87,8 @@
 //TODO: this is a work in progress, need to work on scaling and centering the frequency views
 - (void)updateBarFrames {
 	//Layout the bars based on the updated view frame
-	int tempScaleWidth = 140;//Until we get better centering etc..
-	int scaling = 8;//to make the bars bigger -temp
-	float barWidth = self.frame.size.width / (_barViews.count - tempScaleWidth);
+	int scaling = 7;//to make the bars taller -temp
+	float barWidth = self.frame.size.width/_barViews.count;
 	
 	for(int i = 0;i < _barViews.count; i++){
 		UIView* barView = [_barViews objectAtIndex:i];
@@ -81,8 +96,11 @@
 		float viewHeight = self.frame.size.height;
 		if (_frequencyValues.count > i) {
 			barHeight = viewHeight * [[_frequencyValues objectAtIndex:i] floatValue];
+			barHeight = barHeight * scaling;
+			if(barHeight>viewHeight)
+				barHeight = viewHeight - 5;//max height for bar
 		}
-			barView.frame = CGRectMake(i * barWidth,viewHeight - (barHeight  * scaling), barWidth, barHeight * scaling);
+			barView.frame = CGRectMake(i * barWidth,viewHeight - barHeight, barWidth, barHeight);
 	}
 	
 	
